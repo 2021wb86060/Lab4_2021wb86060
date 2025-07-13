aa2a7037f4aa3ab0b2d7d5c7e5270e51e9df0500
2021wb86060 <2021wb86060@wilp.bits-pilani.ac.in>
2025-07-14 02:57:14 +0530

    Update and rename TestApp.java to FinalTestApp.java

---
+++ src/main/java/com/example/FinalTestApp.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class FinalTestApp {
+    public static void main(String[] args) {
+        System.out.println("Hello from Final output of lab4 Maven Project!");
+    }
+}
--- src/main/java/com/example/TestApp.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class TestApp {
-    public static void main(String[] args) {
-        System.out.println("Hello from lab4 Maven Project!");
-    }
-}
